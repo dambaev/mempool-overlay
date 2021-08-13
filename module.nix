2f1801e5e82a8b1842f3eaffbb87c5a06721d79f
Dambaev Alexander <ice.redmine@gmail.com>
2021-08-13 13:58:39 +0800

    add make into deps of container

--- module.nix
+++ module.nix
@@ -115,6 +115,7 @@ in
         environment.systemPackages = with pkgs; [
           nodejs
           python3
+          make
         ];
         networking.hostName = "mempoolbackendbuilder";
       };
