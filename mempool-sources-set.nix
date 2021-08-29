87668b7ef2724e104676eb08ded5b0bec7270dfd
Dambaev Alexander <ice.redmine@gmail.com>
2021-08-29 18:18:45 +0800

    mempool-sources-set.nix: bump mempool version to 2.2.1

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/mempool/mempool/archive/refs/tags/v2.2.0.tar.gz";
-  sha256 = "1h2vf54rjsajiy7lh0md8ymdipn04qpbxzqqwfr3b13fzfk29wng";
+  url = "https://github.com/mempool/mempool/archive/refs/tags/v2.2.1.tar.gz";
+  sha256 = "1ir63dsq1asn7zg5453vbxbils1p6kjp0vb85i7rdwr6fby9rh0l";
 }
