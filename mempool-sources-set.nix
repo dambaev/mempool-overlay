35ba836bdfbbe0b2ec8bc02ee4214601b1e2cc45
Dambaev Alexander <ice.redmine@gmail.com>
2021-09-23 17:39:30 +0800

    mempool-sources-set.nix: switch to a fork repo

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/mempool/mempool/archive/refs/tags/v2.2.2-rc2.tar.gz";
+  url = "https://github.com/dambaev/mempool/archive/refs/tags/v2.2.2-rc2-exposed-chainwork.tar.gz";
   sha256 = "0jw1r0ksdn5z4vpkhmy6c7v7y0cmbzm74wrivxrysnx0y3dfyi6z";
 }
