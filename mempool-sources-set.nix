6a521e609230f0ecae6d070dfae79a466038b622
Dambaev Alexander <ice.redmine@gmail.com>
2021-09-26 14:49:30 +0800

    mempool-sources-set.nix: use the latest commit

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/dambaev/mempool/archive/refs/tags/v2.2.2-rc2-exposed-chainwork.tar.gz";
-    sha256 = "1blx06v4vw8i0yfi1lmgqb7vg0jil7lfdsxfl6pw9dyyvikkiis8";
+  url = "https://github.com/dambaev/mempool/archive/73ef7c7b4c6d66f0f6368d7465b9aba74c59f2f4.tar.gz";
+  sha256 = "0b92641f4m38ildgbki04h929m532b6bk4gwd36g0k1fwjvhv0pj";
 }
