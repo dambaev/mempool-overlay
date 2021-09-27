e1e2a8030b4be2d892645a7bf4471168b04da432
Dambaev Alexander <ice.redmine@gmail.com>
2021-09-27 14:57:25 +0800

    mempool-sources-set.nix: use commit with replaced logos

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://github.com/dambaev/mempool/archive/73ef7c7b4c6d66f0f6368d7465b9aba74c59f2f4.tar.gz";
-  sha256 = "0b92641f4m38ildgbki04h929m532b6bk4gwd36g0k1fwjvhv0pj";
+  url = "https://github.com/dambaev/mempool/archive/2c1ec141f6770bfdcfa30f07f0af53f9dc34bb3c.tar.gz";
+  sha256 = "09cwbczfy3kr3asb70rzgn5dmgdfynmw7sbscd6mvaabmfjp6hyf";
 }
