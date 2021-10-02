bcba8e89a6139e74382fdfcdbe68acb7306ed8dc
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-02 13:01:13 +0800

    mempool-sources-set.nix: add comments

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,3 +1,4 @@
+# this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
   url = "https://github.com/dambaev/mempool/archive/95f0d070bb9d906047cfeca35e9dfacd0f89854a.zip";
   sha256 = "0ni6xkc8qi2118pa6369ay5rg24aiqiqx0lsxicvhl468jd9fz0x";
