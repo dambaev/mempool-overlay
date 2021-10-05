c99102f4a2f9c95250c83ead82a18ffccf3cd0da
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-06 05:22:33 +0800

    show posix time in seconds on the details page

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/169838d7a5a2cf5276ec5e7b115da97cda0e4cd0.zip";
-  sha256 = "0nq6vymm7lkw18q93kxhn5a8l75ng16mpc1nw2rbafr7hi2k3bxl";
+  url = "https://github.com/dambaev/mempool/archive/3be6a8bbf7c3d24c8e4395ab91a0904cedea58c0.zip";
+  sha256 = "1qdwxziw2yhgddcgihhhpj5h726y3v17nn96lm1ssy5m4xkd99dp";
 }
