ccc5c6af76db259ee2da2011035b580e67d20385
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-09 22:52:09 +0800

    replace chainwork with mediantime in the scrolling cubes area

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/3be6a8bbf7c3d24c8e4395ab91a0904cedea58c0.zip";
-  sha256 = "1qdwxziw2yhgddcgihhhpj5h726y3v17nn96lm1ssy5m4xkd99dp";
+  url = "https://github.com/dambaev/mempool/archive/9ed1d6aaaa6772c48e7349fc19d7be6b470f3f34.zip";
+  sha256 = "0j2flji4qajhfs9hs74661hfjcwlb8pspk5phmy07zhqjnjw59m1";
 }
