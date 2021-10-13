6abdd7a5b9fd7d5e88c997a08e7b6a258b04249c
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-13 13:22:33 +0000

    mempool-sources-set.nix: sync with upstream

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/279fdb4241b968e73f3e3323312b66099563b178.zip";
-  sha256 = "1c1wcj97ldc8ly2lq1wdrac5ir7sjl2pmdbqj5bff3zqmf74ddfv";
+  url = "https://github.com/dambaev/mempool/archive/a3651171f3f483a9ca46ccd78c7135f1c95ac1cc.zip";
+  sha256 = "1m7njvqhbc7wxg5v0w7s12x050jjnd9fqh5fdz5sy4855ap4h0hg";
 }
