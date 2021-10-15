0220468e6baed014730d95e24504106e623d6977
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-16 03:40:56 +0800

    frontend: remove terms of service link from about page

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/a4b12beaa78962180429ca391d12806bce8730d3.zip";
-  sha256 = "0964k12am9icsim45hlm6daph2alpy0i059wsxmr0d7apc5g8k98";
+  url = "https://github.com/dambaev/mempool/archive/b9c59841337227f9d72b1f7eef55e1203b9085e3.zip";
+  sha256 = "00617m9a726rsl6pd8glmn9ykhb4lksxmxllpbjxx5y32yqsjq76";
 }
