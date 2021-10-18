65a7ce475579ed30f18d9dda351f3dfcabfdba41
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-18 12:10:54 +0000

    frontend: replace title

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/0564e5593a539de8fa126552594e142f8bfc6bce.zip";
-  sha256 = "0cfmbnpdb8fxnjvl86j4g39xqb2i3r2iqyh9x97i7mdxyriifx87";
+  url = "https://github.com/dambaev/mempool/archive/bdcb1df0f4f7e156b49839505e8725ae3592e3fe.zip";
+  sha256 = "0mvazaldfiwdr0h395ybvb9d10pj9p3y29isnmxqg3lv1fslg6zq";
 }
