dec12beb61a1d5daf3ca1f3a62beb2429e4583e7
Dambaev Alexander <ice.redmine@gmail.com>
2021-10-23 03:48:26 +0000

    mempool-sources-set.nix: do not prepare blocks before using getInitData

--- mempool-sources-set.nix
+++ mempool-sources-set.nix
@@ -1,5 +1,5 @@
 # this file is being imported from multiple modules and it's goal is to provide single source of the op-exchange's sources
 {
-  url = "https://github.com/dambaev/mempool/archive/a40cd85fa44653e12d5cc6ece204a547e6a99108.zip";
-  sha256 = "0bhigfv0w4r9jf01iirj0l5jai5nschmdw0yw4d4s44yaffhms42";
+  url = "https://github.com/dambaev/mempool/archive/02bcfc50c2374051505feaaf68420038e34bc145.zip";
+  sha256 = "1d6lz769rgg00nyqyhq1ckr3v23l5alfsn7hgp9wkhy8jjr4x2s4";
 }
